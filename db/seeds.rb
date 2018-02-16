2e533127a25731273c315d1b2679470f60f720b3
Konstantinos Mitsainas <mitsenask@gmail.com>
2018-02-16 17:23:39 +0100

    add db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Ingredient.create(name: "lemon")
-Ingredient.create(name: "ice")
-Ingredient.create(name: "mint leaves")
+require 'open-uri'
+require 'json'
+Cocktail.destroy_all
+Ingredient.destroy_all
 
-Cocktail.create(name: "Mojito")
+
+response = open('http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list').read
+ingredient = JSON.parse(response)
+
+list = ingredient["drinks"]
+
+list.each do |hash|
+ value =  hash.values[0]
+ ingredient = Ingredient.create(name: value)
+end
+
